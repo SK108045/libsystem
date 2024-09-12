0452dad2e9eb146cdfeabf7960de1be8cd5159e2
SK10 <johnwakaba146@gmail.com>
2024-09-12 12:17:02 +0300

    Initial commit

---
+++ admin/includes/conn.php
@@ -0,0 +1,8 @@
+<?php
+	$conn = new mysqli('sql209.thsite.top', 'thsi_36612395', 'nljswc?Q', 'thsi_36612395_libsystem',3306);
+
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
+	
+?>
